0634790d58f34af9866cd1f4e48e1b9d672103af
tranvid25 <lsdnh041004@gmail.com>
2025-08-12 15:08:20 +0700

    feat/Banner

--- Ecommerce-project/src/App.jsx
+++ Ecommerce-project/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 
 import './App.css';
-import Header from '@components/Header/Header';
-
-
+import HomePage from '@components/HomePage/HomePage';
 function App() {
   return (
     <>
-      <Header></Header>
+      <HomePage></HomePage>
     </>
   );
 }
