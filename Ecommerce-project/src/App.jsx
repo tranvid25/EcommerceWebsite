95dbf4013826204dbc2eaa00a26ee109838c7192
tranvid25 <lsdnh041004@gmail.com>
2025-08-13 14:37:17 +0700

    feat/section

--- Ecommerce-project/src/App.jsx
+++ Ecommerce-project/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 
 import './App.css';
 import HomePage from '@components/HomePage/HomePage';
+import Info from '@components/Info/Info';
 function App() {
   return (
     <>
       <HomePage></HomePage>
+      <Info/>
     </>
   );
 }
---
+++ Ecommerce-project/src/components/Info/constant.js
@@ -0,0 +1,27 @@
+import truckicon from '@icon/truckicon.min.svg';
+import boxicon from '@icon/boxicon.min.svg';
+import chaticon from '@icon/chaticon.min.svg';
+import payicon from '@icon/payicon.min.svg';
+export const dataInfo=[
+    {
+        title:'Fastest Shipping',
+        description:'Order at $39 order',
+        src:truckicon
+    },
+    {
+        title:'100% Safe Payments',
+        description:'9 month installMents',
+        src:payicon
+    },
+    {
+        title:'14-Days Return',
+        description:'Shop with confidence',
+        src:boxicon,
+    },
+    {
+        title:'24/7 Online Support',
+        description:'Delivered to home',
+        src:chaticon
+    },
+
+];
